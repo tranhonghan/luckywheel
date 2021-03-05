15ce29c31db3f646809423261def1146a3ffb7d8
MACBOOK <macbook@MACBOOKs-MacBook-Pro.local>
2021-03-05 16:17:39 +0700

    imp. handle wheel component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {
+  BrowserRouter as Router, Switch, Route, Link
+} from 'react-router-dom'
+import {Home} from './screens/home'
+import {Admin} from './screens/admin'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Switch>
+          <Route path='/home'>
+            <Home/>
+          </Route>
+          <Route path='/admin'>
+            <Admin/>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
